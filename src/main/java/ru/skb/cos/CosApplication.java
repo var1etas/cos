499068de644a22477733f47d04733082ba7f23c2
Alexandr Filatov <filatovalexandr2003@gmail.com>
2024-10-16 14:21:07 +0500

    Add .gitignore

---
+++ src/main/java/ru/skb/cos/CosApplication.java
@@ -0,0 +1,13 @@
+package ru.skb.cos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skb/cos/CosApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skb.cos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
